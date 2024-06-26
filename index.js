c47f55e59c75adacabb92c964dbd4c2bbb270728
fels123 <kelengulienakhro@gmail.com>
2024-06-26 21:06:56 +0530

    factory and constructor function exercise

--- index.js
+++ index.js
@@ -2,13 +2,37 @@
 //create an object with street,city ,zipcode
 //function name showAddress(address)
 
-const address = {
-  street: "goal street",
-  city: "Naput",
-  zipCode: 7846,
-};
+// const address = {
+//   street: "goal street",
+//   city: "Naput",
+//   zipCode: 7846,
+// };
 
-function showAddress(address) {
-  for (let key in address) console.log(key, address[key]);
+// function showAddress(address) {
+//   for (let key in address) console.log(key, address[key]);
+// }
+// showAddress(address);
+
+// Factory and constructor function
+// Exercise -2
+
+//factory function
+// let address = createAddress("a", "b", "c");
+// console.log(address);
+
+// function createAddress(street, city, zipCode) {
+//   return {
+//     street,
+//     city,
+//     zipCode,
+//   };
+// }
+
+//Constructor function
+let address = new Address("a", "b", "c");
+console.log(address);
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
 }
-showAddress(address);
